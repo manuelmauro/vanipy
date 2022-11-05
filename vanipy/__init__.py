7f379a72643d08d6bff47de7008c93faad8da60d
Manuel Mauro <manuel.mauro@protonmail.com>
2022-11-05 10:16:36 +0100

    Accept multiple prefixes in acommand

--- vanipy/__init__.py
+++ vanipy/__init__.py
@@ -2,10 +2,9 @@ import json
 from algosdk import account
 import click
 from rich.console import Console
-from rich.table import Table
 
 from vanipy.query import Query
-from vanipy.result import Result
+from vanipy.result import Result, result_table
 
 
 @click.group(help="A tool for very superficial Algorand addresses.")
@@ -17,41 +16,31 @@ def vanipy(ctx):
 
 
 @vanipy.command(help="Generate a vanity address with a given prefix.")
-@click.argument("prefix")
+@click.argument("prefixes", nargs=-1)
 @click.option("--json", "json_", is_flag=True, help="Output as JSON.")
 @click.pass_context
-def prefix(ctx, prefix, json_):
-    query = Query(q=prefix)
-    if not query.is_valid():
-        print("Invalid pattern. Please, specify a base32 prefix.")
-        return 1
+def prefix(ctx, prefixes, json_):
+    results = []
+    for prefix in prefixes:
+        query = Query(q=prefix)
+        if not query.is_valid():
+            print("Invalid pattern. Please, specify a base32 prefix.")
+            return 1
 
-    vanity_pk, vanity_address = account.generate_account()
-
-    while (not vanity_address.startswith(prefix)):
         vanity_pk, vanity_address = account.generate_account()
+        while (not vanity_address.startswith(prefix)):
+            vanity_pk, vanity_address = account.generate_account()
 
-    result = Result(pk=vanity_pk, address=vanity_address)
+        results.append(Result(pk=vanity_pk, address=vanity_address))
 
     if json_:
-        print(result.json())
-
+        json_out = json.dumps([r.dictify() for r in results])
+        print(json_out)
         return 0
 
-    table = Table(title="Loads of Vanity")
-
-    table.add_column("Address", style="magenta")
-    table.add_column("Private key",
-                     justify="right",
-                     style="cyan",
-                     no_wrap=True)
-    table.add_column("Mnemonic", justify="right", style="green")
-
-    table.add_row(result.address, result.pk, result.mnemonic())
-
+    table = result_table(results)
     console = Console()
     console.print(table)
-
     return 0
 
 
--- vanipy/result.py
+++ vanipy/result.py
@@ -1,6 +1,7 @@
 from algosdk import mnemonic
 from dataclasses import dataclass
 import json
+from rich.table import Table
 
 
 @dataclass
@@ -15,8 +16,21 @@ class Result():
             "mnemonic": self.mnemonic(),
         }
 
-    def json(self) -> str:
-        return json.dumps(self.dictify())
-
     def mnemonic(self) -> str:
         return mnemonic.from_private_key(self.pk)
+
+
+def result_table(results: list[Result]) -> Table:
+    table = Table(title="Loads of Vanity", show_lines=True)
+
+    table.add_column("Address", style="magenta")
+    table.add_column("Private key",
+                     justify="right",
+                     style="cyan",
+                     no_wrap=True)
+    table.add_column("Mnemonic", justify="right", style="green")
+
+    for result in results:
+        table.add_row(result.address, result.pk, result.mnemonic())
+
+    return table
